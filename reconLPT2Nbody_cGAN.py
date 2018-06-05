71e23d3fec0c49d897deb929104a93fef0886690
Siyu He <siyuh@workergpu12.cm.cluster>
2018-06-05 14:55:17 -0400

    add cgan

--- reconLPT2Nbody_cGAN.py
+++ reconLPT2Nbody_cGAN.py
@@ -3,11 +3,13 @@ import argparse
 import torch
 import torch.nn as nn
 import time
-from encoder_decoder import BasicBlock, Lpt2NbodyNet
-from discriminator import LPT2NbodyDiscriminator
 from data_utils import SimuData
 from torch.utils.data import DataLoader
+import sys
+sys.path.insert(0, '/mnt/home/siyuh/Project/Recon/c-gan/')
 from cgan import CGAN
+from encoder_decoder import BasicBlock, Lpt2NbodyNet
+from discriminator1 import LPT2NbodyDiscriminator
 import numpy as np
 
 # python reconLPT2Nbody_cGAN.py --config_file_path configs/config.json
@@ -23,14 +25,15 @@ if __name__ == "__main__":
 	with open(args.config_file_path) as f:
 		configs = json.load(f)
 
-	generator = Lpt2NbodyNet(BasicBlock, [3, 3])
+	generator = Lpt2NbodyNet(BasicBlock, [3, 3],configs["net_params"]["leaky_param"])
 	generator.cuda()
 	l2_criterion = nn.MSELoss()
 	entropy_criterion = nn.BCELoss()
 	base_data_path = configs["base_data_path"]
 	output_path = configs["output_path"]
 	in_channels = 6 if configs["train"]["cgan"]["conditioning"] else 3
-	discriminator = LPT2NbodyDiscriminator(BasicBlock, [3, 3, 3], in_channels)
+	#discriminator = LPT2NbodyDiscriminator(BasicBlock, [1, 1, 1], in_channels)
+	discriminator = LPT2NbodyDiscriminator(in_channels)
 	discriminator.cuda()
 
 	TrainSet = SimuData(base_data_path,
@@ -52,48 +55,59 @@ if __name__ == "__main__":
 	ValLoader   = DataLoader(ValSet,
 								batch_size=configs['val']['batch_size'],
 								shuffle=False,
-								num_workers=configs['train']['num_workers'])
+								num_workers=configs['val']['num_workers'])
 	TestLoader  = DataLoader(TestSet,
 								batch_size=configs['test']['batch_size'],
 								shuffle=False,
-								num_workers=configs['train']['num_workers'])
+								num_workers=configs['test']['num_workers'])
 	net = CGAN(
 		generator,
 		discriminator,
 		configs["train"]["cgan"]
 	)
+	net = torch.load(output_path+"BestModel.pt")
 	eval_frequency = configs["train"]["eval_frequency"]
 	loss_val = []
 	best_validation_accuracy = 100
-
-	for _ in range(configs['train']['num_epoches']):
-		for t, data in enumerate(TrainLoader, 0):
-			start_time = time.time()
-			batch_x = torch.autograd.Variable(data[0], requires_grad=False).cuda()
-			batch_y = torch.autograd.Variable(data[1], requires_grad=False).cuda()
-			net.train()
-			net.train_discriminator_step(batch_x, batch_x, batch_y)
-			net.train_generator_step(batch_x,
-				batch_y,
-				extra_loss=l2_criterion,
-				extra_loss_fraction=configs["train"]["cgan"]["extra_loss_fraction"])
-			if (t != 0 and t % eval_frequency == 0):
-				net.eval()
+	iterTime=3501
+	if(configs["is_train"]):
+		for _ in range(configs['train']['num_epoches']):
+			for t, data in enumerate(TrainLoader, 0):
 				start_time = time.time()
-				_loss = 0
-				for t_val, data in enumerate(ValLoader,0):
-					g_batch_x = torch.autograd.Variable(data[0],requires_grad=False,volatile=True).cuda()
-					y_pred = net.forward_generator(g_batch_x)
-					_loss += l2_criterion(y_pred, torch.autograd.Variable(data[1],requires_grad=False).cuda()).data[0]
-				loss_val.append(_loss/t_val)		
-				np.savetxt(output_path+'valLoss.txt',loss_val)
-				print ('valid: ' + str(_loss/t_val))
-				"""
-				if( _loss/t_val < best_validation_accuracy):
-					torch.save(net,output_path+'BestModel.pt')
-				if(iterTime%500==0 and iterTime!=0):
-					torch.save(net,output_path+'BestModel'+str(iterTime)+'.pt')
-				"""
-			print(t)
+				batch_x = torch.autograd.Variable(data[0], requires_grad=False).cuda()
+				batch_y = torch.autograd.Variable(data[1], requires_grad=False).cuda()
+				net.train()
+				net.train_discriminator_step(batch_x, batch_x, batch_y)
+				net.train_generator_step(batch_x,
+					batch_y,
+					extra_loss=l2_criterion,
+					extra_loss_fraction=configs["train"]["cgan"]["extra_loss_fraction"])
+				if (iterTime != 0 and iterTime % eval_frequency == 0):
+					net.eval()
+					start_time = time.time()
+					_loss = 0
+					for t_val, data in enumerate(ValLoader,0):
+						with torch.no_grad():
+							g_batch_x = torch.autograd.Variable(data[0],requires_grad=False).cuda()
+							y_pred = net.forward_generator(g_batch_x)
+							_loss += l2_criterion(y_pred, torch.autograd.Variable(data[1],requires_grad=False).cuda()).item()
+					loss_val.append(_loss/t_val)		
+					np.savetxt(output_path+'valLoss1.txt',loss_val)
+					print ('valid: ' + str(_loss/t_val))
+					if( _loss/t_val < best_validation_accuracy):
+						torch.save(net,output_path+'BestModel_1.pt')
+					if(iterTime%500==0 and iterTime!=0):
+						torch.save(net,output_path+'BestModel'+str(iterTime)+'.pt')
+				iterTime +=1
+	if(configs["is_test"]):
+		net = torch.load(output_path+"BestModel.pt")
+		net.eval()
+		for t_val, data in enumerate(TestLoader,0):
+			g_batch_x = torch.autograd.Variable(data[0],requires_grad=False).cuda()
+			y_pred = net.forward_generator(g_batch_x)
+			np.save(output_path+'test_'+str(t_val)+'.npy',np.concatenate((np.squeeze(y_pred.data.cpu().numpy()),np.squeeze(data[1].numpy())),axis=0))
+			if(t_val==5):
+				break
 
+	
 
